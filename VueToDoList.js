a77c2e926e853be87f7415db8f3327b6c6d2e56d
unknown <vtruglio@deakin.edu.au>
2022-08-03 10:45:46 +1000

    Addition of Task 5 to Week 1

---
+++ VueToDoList.js
@@ -0,0 +1,10 @@
+let vue = new Vue({
+    el: "#app",
+    data: {
+        todos: [
+            {title: 'SIT120 Assignment 1', checked: true},
+            {title: 'SIT120 Assignment 2', checked: false},
+            {title: 'SIT120 Assignment 3', checked: false},
+        ],
+    },
+});
